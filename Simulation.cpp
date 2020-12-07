da9d585444244d36e4225082f8912aa113c79561
Rudolf Schoenmann <rudolf.schoenmann@emft-extern.fraunhofer.de>
2020-12-07 21:21:17 +0100

    replace wrong headers

--- Simulation.cpp
+++ Simulation.cpp
@@ -1,3 +1,22 @@
+/*
+Program:     ContaminationFlow
+Description: Monte Carlo simulator for satellite contanimation studies
+Authors:     Rudolf Schönmann / Hoai My Van
+Copyright:   TU Munich
+Forked from: Molflow (CERN) (https://cern.ch/molflow)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+Full license text: https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
+*/
 #include "Simulation.h"
 #include "IntersectAABB_shared.h"
 
